2eb0d39d7e5c74dab76b631055299dd9404123c9
kiaradexm <alex@alex.com>
2020-04-12 06:35:12 -0500

    Oracle DB sql sentences

---
+++ 2.sql
@@ -0,0 +1,88 @@
+--BIENVENIDOS
+--IMPORTANTE PERO OPCIONAL (VER VIDEO DEL CANAL DE NOMBRE: Primeros Pasos Oracle 18c Express Edition.)
+
+
+--CREAREMOS UN NUEVO USUARIO CON SUS ROLES, SU TABLESPACE, CONEXION 
+
+---1).	VER EL VIDEO (MUY IMPORTANTE)
+
+--0).	Comprobar que los servicios windows OracleServiceXe y OracleOraDB18Home1TNSListener est�n iniciados.
+
+--1).	Tener instalado Oracle 18c Expres Edition. (OMITAN ESTE)
+--2).	cmd (Ejecutar como Administrador)
+--3).	sqlplus
+--4).	sys /as sysdba
+--5).	Ver bases de datos en la que nos podemos conectar.
+	SELECT name, con_id FROM v$pdbs;
+
+
+--6).	Cambiaremos a la sesi�n de nuestra base de datos predeterminada: (PASO IMPORTANTE)
+	ALTER SESSION SET container = XEPDB1;
+
+--7).	Ver si nuestra base de datos predeterminada est� en estado ABIERTA: 
+	SELECT name, open_mode FROM v$pdbs;
+	
+
+--8).	Abrir base de datos (En caso de que no aparezca abierta):
+	ALTER PLUGGABLE DATABASE open;
+
+
+--9).	Creamos TABLESPACE de nombre: SAMPLETAB
+	CREATE SMALLFILE TABLESPACE SAMPLETAB DATAFILE 'C:\app\alexander\product\18.0.0\oradata\XE\XEPDB1\SAMPLETAB.DBF' SIZE 200M LOGGING EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT AUTO;
+	CREATE SMALLFILE TABLESPACE SAMPLETAB2 DATAFILE 'C:\app\alexander\product\18.0.0\oradata\XE\XEPDB1\SAMPLETAB2.DBF' SIZE 200M LOGGING EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT AUTO;
+	
+	
+	CREATE SMALLFILE TABLESPACE SPACEBIBLIOTECA DATAFILE 'C:\app\alexander\product\18.0.0\oradata\XE\XEPDB1\SPACEBIBLIOTECA.DBF' SIZE 200M LOGGING EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT AUTO;
+
+
+--10).	Creamos nuestro usuario de nombre: SAMPLEUSER, perfil por defecto: DEFAULT, le ponemos de contrase�a: oracle, 
+--	asignamos tablespace por defecto: SAMPLETAB, tablespace temporal: TEMP, desbloqueamos el usuario y le damos una quota ilimitada a nuestro tablespace.
+
+	CREATE USER SAMPLEUSER PROFILE DEFAULT IDENTIFIED BY oracle DEFAULT TABLESPACE SAMPLETAB TEMPORARY TABLESPACE TEMP ACCOUNT UNLOCK quota unlimited on SAMPLETAB;
+	CREATE USER BIBLIOTECA PROFILE DEFAULT IDENTIFIED BY oracle DEFAULT TABLESPACE SAMPLETAB2 TEMPORARY TABLESPACE TEMP ACCOUNT UNLOCK quota unlimited on SAMPLETAB2;
+	
+	
+	CREATE USER LIBRARY PROFILE DEFAULT IDENTIFIED BY oracle DEFAULT TABLESPACE SPACEBIBLIOTECA TEMPORARY TABLESPACE TEMP ACCOUNT UNLOCK quota unlimited on SPACEBIBLIOTECA;
+
+
+--11).	Concedemos los roles de CONNECT y RESOURCE a nuestro usuario
+
+	GRANT CONNECT TO BIBLIOTECA;
+	GRANT RESOURCE TO BIBLIOTECA;
+	GRANT INSERT ANY TABLE TO BIBLIOTECA;
+
+
+	GRANT CONNECT TO LIBRARY;
+	GRANT RESOURCE TO LIBRARY;
+	GRANT INSERT ANY TABLE TO LIBRARY;
+	GRANT CREATE ANY VIEW TO LIBRARY;
+
+--12).	Nos aseguramos de estar en nuestro esquema:
+	ALTER SESSION SET CURRENT_SCHEMA = LIBRARY;
+
+
+--13).	Nos conectamos con nuestro usuario:
+--	conn SAMPLEUSER/oracle@//DESKTOP-LUCJFIV:1521/xepdb1
+
+--	tambi�n podemos conectarnos usando el ALIAS agregado anteriormente al archivo tnsnames.ora
+--	conn SAMPLEUSER/oracle@XEPDB1; (Esto lo pueden ver en el video: Primeros Pasos Oracle 18c Express Edition.)
+	
+
+--14).	Ver las tablas del esquema SAMPLEUSER:
+	select table_name from user_tables;
+
+
+--15).	Ver usuario de conexi�n actual:
+	SHOW USER;
+
+
+--16).	AHORA NOS CONECTAREMOS CON PL/SQL Developer.
+
+
+--17).	Crearemos las tablas utilizando el archivo de texto: CREATE_TABLES.txt que est� en nuestra carpeta descargada.
+
+
+--18).	Insertaremos los datos utilizando el archivo de texto: INSERTS.txt que est� en nuestra carpeta descargada.
+
+
+
---
+++ basehoteles.sql
@@ -0,0 +1,222 @@
+USE hoteles;
+
+
+CREATE TABLE cargo (
+    codigo int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    PRIMARY KEY (CODIGO)
+);
+
+CREATE TABLE empleado (
+    cedula int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    cargo int,
+    PRIMARY KEY (cedula)
+);
+
+CREATE TABLE cliente (
+    cedula int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    apellido varchar(255) NOT NULL,
+    PRIMARY KEY (cedula)
+);
+
+CREATE TABLE habitacion (
+    numero int NOT NULL,
+    precio int NOT NULL,
+    PRIMARY KEY (numero)
+);
+
+CREATE TABLE caracteristica(
+	codigo int,
+    description varchar(250),
+    PRIMARY KEY (codigo)
+);
+
+CREATE TABLE habitacion_caracteristica(
+	num_habitacion int,
+    codigo_caracterisca int,
+    PRIMARY KEY (num_habitacion,codigo_caracterisca)
+);
+
+CREATE TABLE empleado_habitacion (
+    cedula_empleado int NOT NULL,
+    numero_habitacion int NOT NULL,
+    PRIMARY KEY (cedula_empleado,numero_habitacion),
+    FOREIGN KEY (cedula_empleado) REFERENCES empleado(cedula),
+	FOREIGN KEY (numero_habitacion) REFERENCES habitacion(numero)
+);
+CREATE TABLE actividad (
+    codigo int,
+    nombre varchar(255) NOT NULL,
+    cod_cargo int,
+    PRIMARY KEY (codigo),
+	FOREIGN KEY (cod_cargo) REFERENCES cargo(codigo)
+);
+
+CREATE TABLE reserva (
+    codigo int,
+    cedula_cliente int NOT NULL,
+    fecha_inicio date,
+    fecha_fin date,
+    fecha_entrega date,
+    PRIMARY KEY (codigo),
+	FOREIGN KEY (cedula_cliente) REFERENCES cliente(cedula));
+
+CREATE TABLE habitacion_cliente(
+    cedula_cliente int,
+    id_reserva int,
+    num_habitacion int,
+    PRIMARY KEY (cedula_cliente,id_reserva,num_habitacion)
+);
+-- INSERT DATA
+
+-- RESERVAS
+
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('1', '1', TO_DATE('2020-03-10 23:27:40', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-19 23:27:54', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-18 23:28:08', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('2', '2', TO_DATE('2020-03-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-19 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-18 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('3', '3', TO_DATE('2020-03-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-19 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-18 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('4', '4', TO_DATE('2020-03-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-19 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-18 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('5', '5', TO_DATE('2020-03-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-19 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-03-18 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('6', '1', TO_DATE('2019-07-17 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-06-21 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2019-06-21 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."RESERVA" (CODIGO, CEDULA_CLIENTE, FECHA_INICIO, FECHA_FIN, FECHA_ENTREGA) VALUES ('7', '1', TO_DATE('2020-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-01-15 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2020-01-15 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+
+
+-- HabitacionCliente
+INSERT INTO habitacion_cliente VALUES(1, 1, 101);
+INSERT INTO habitacion_cliente VALUES(1, 6, 101);
+INSERT INTO habitacion_cliente VALUES(1, 6, 102);
+INSERT INTO habitacion_cliente VALUES(1, 6, 202);
+INSERT INTO habitacion_cliente VALUES(1, 7, 101);
+INSERT INTO habitacion_cliente VALUES(2, 2, 101);
+INSERT INTO habitacion_cliente VALUES(3, 3, 202);
+INSERT INTO habitacion_cliente VALUES(4, 4, 301);
+
+-- Clientes
+INSERT INTO cliente VALUES(1, 'Juan', 'Nilo');
+INSERT INTO cliente VALUES(2, 'Zion', 'Rofola');
+INSERT INTO cliente VALUES(3, 'Barta', 'Hoyos');
+INSERT INTO cliente VALUES(4, 'Kamila', 'Smatj');
+INSERT INTO cliente VALUES(5, 'Pepe', 'Dokim');
+
+# Valores para cargo
+
+INSERT INTO cargo VALUES(1, 'Habitacion');
+INSERT INTO cargo VALUES(2, 'Masajista');
+INSERT INTO cargo VALUES(3, 'Mantenimiento');
+INSERT INTO cargo VALUES(4, 'Contador');
+INSERT INTO cargo VALUES(5, 'Protocolo');
+# Valores para actividad
+
+INSERT INTO actividad VALUES(1, 'Limpieza', 1);
+INSERT INTO actividad VALUES(2, 'Cambio_Toallas', 1);
+INSERT INTO actividad VALUES(3, 'Lavanderia', 1);
+
+# Valores para Habitacion
+
+INSERT INTO habitacion VALUES(101, 50000);
+INSERT INTO habitacion VALUES(102, 75000);
+INSERT INTO habitacion VALUES(201, 100000);
+INSERT INTO habitacion VALUES(202, 150000);
+INSERT INTO habitacion VALUES(301, 350000);
+
+# Valores para Empleado
+
+
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('1010', 'Alexander', '1', 'Arevalo', TO_DATE('2020-04-07 22:47:41', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('2010', 'Jaime', '1', 'Garzon', TO_DATE('2003-04-25 22:47:55', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('3010', 'Paco', '1', 'Amarilo', TO_DATE('2003-04-25 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('4010', 'Yuri', '1', 'Pachon', TO_DATE('2003-04-25 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('5010', 'Kiara', '2', 'Morado', TO_DATE('1999-04-18 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+INSERT INTO "SAMPLEUSER"."EMPLEADO" (CEDULA, NOMBRE, CARGO, APELLIDO, FECHA_NACIMIENTO) VALUES ('6010', 'Paco', '3', 'Amarilo', TO_DATE('2003-04-27 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+
+# Valores para EmpleadohABITACION
+
+INSERT INTO empleado_habitacion VALUES(1010, 101);
+INSERT INTO empleado_habitacion VALUES(2010, 102);
+INSERT INTO empleado_habitacion VALUES(3010, 201);
+INSERT INTO empleado_habitacion VALUES(4010, 202);
+INSERT INTO empleado_habitacion VALUES(4010, 301);
+
+
+# Valores para CARACTERISTICA
+
+INSERT INTO caracteristica VALUES(1, 'Baño');
+INSERT INTO caracteristica VALUES(3, 'Bar');
+INSERT INTO caracteristica VALUES(2, 'Salon');
+INSERT INTO caracteristica VALUES(4, 'Hidromasaje');
+# Modificar tabla empleado
+
+ALTER TABLE Empleado add apellido varchar(200);
+ALTER TABLE Empleado add fecha_nacimiento date;
+
+
+
+# Valores para HabitacionCaracteristica
+
+-- 1 baño 2 bar 3 Salon 4 Hidromasaje
+INSERT INTO habitacion_caracteristica VALUES(101, 1);
+INSERT INTO habitacion_caracteristica VALUES(102, 1);
+INSERT INTO habitacion_caracteristica VALUES(201, 1);
+INSERT INTO habitacion_caracteristica VALUES(202, 1);
+INSERT INTO habitacion_caracteristica VALUES(301, 1);
+
+INSERT INTO habitacion_caracteristica VALUES(201, 2);
+INSERT INTO habitacion_caracteristica VALUES(202, 2);
+INSERT INTO habitacion_caracteristica VALUES(301, 2);
+
+INSERT INTO habitacion_caracteristica VALUES(202, 3);
+INSERT INTO habitacion_caracteristica VALUES(301, 3);
+
+INSERT INTO habitacion_caracteristica VALUES(301, 4);
+# Update un campo Y mayuscula acepta el año en 4 DIgitos 
+
+UPDATE Empleado SET fecha_nacimiento =STR_TO_DATE("01/02/2001","%m/%d/%Y") WHERE cedula = 1015;
+
+UPDATE Empleado SET fecha_nacimiento =STR_TO_DATE("01/02/2001","%m/%d/%Y") WHERE cedula = 2015;
+
+--DELETE
+DELETE FROM cargo WHERE codigo = 5;
+
+
+/*ORACLE SENTENCES*/
+--Conexion a mi User HAY QUE CREAR USERS al hilo principal que es xepdb1
+conn SAMPLEUSER/oracle@//DESKTOP-M966U1C:1521/xepdb1
+
+conn SAMPLEUSER/oracle@XEPDB1;
+
+
+--Ver mis tablas con ORACLE
+ select table_name from user_tables;
+
+
+
+
+ --consultas
+
+ --Nombre del cargo y la cantidad de empleados para cada uno de los cargos.
+
+select cargo.nombre, COUNT(empleado.cargo) AS cant_empleados from cargo
+INNER JOIN empleado ON cargo.codigo = empleado.cargo
+GROUP BY cargo.nombre; 
+
+--Habitaciones a cargo del empleado Lalo Rodriguez
+select * from empleado_habitacion where cedula_empleado = 4010;
+
+-- Cliente que ha alquilado mayor numero de Habitaciones
+SELECT cedula_cliente, COUNT( cedula_cliente ) AS total_habs
+FROM  habitacion_cliente
+GROUP BY cedula_cliente;
+
+
+--. Valor a pagar para una determinada reserva.
+SELECT * FROM reserva WHERE codigo = 6;
+SELECT * FROM habitacion_cliente where id_reserva = 6;
+SELECT * FROM habitacion;
+
+
+-- Mirar los dias que se quedo ?
+
+SELECT DATEDIFF(day,  fecha_incio,fecha_entrega) from reserva where codigo = 6;
+
+-- Hacer operacion  dias * valor_habitacy sumar todas las las multiplicaiones
