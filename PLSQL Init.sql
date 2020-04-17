6f288c470e9eac9585009cb0dffdeb52f4bf0cd4
nova <alex@alex.com>
2020-04-17 18:20:01 -0500

    Se add PL/SQL file

---
+++ PLSQL Init.sql
@@ -0,0 +1,76 @@
+/* */
+SET SERVEROUTPUT ON; /* Habilitar Consola*/
+DECLARE
+    /* Declarar variables*/
+    --mensaje VARCHAR(30) := 'Texto tipo string'; 
+    nombre VARCHAR(30) := 'Manuel'; 
+    edad INT;
+BEGIN
+    
+    /*Capturar por Teclado String (OJO COMILLAS EN STRING)*/
+    nombre := '&Nombre';
+    /* Caputar Int*/
+      edad := &Edad;
+    
+     --DBMS_OUTPUT.put_line ('Esto imprime por consola, Always comiilla simple');
+     --DBMS_OUTPUT.put_line (mensaje || ' JUAN ' || nombre); /* Concatenar*/
+    DBMS_OUTPUT.put_line ('Tu edad es: ' || edad); /* Concatenar*/
+END;
+
+
+/*LERR DOS NUMERO E IMPRIMIR SUMA*/
+
+DECLARE
+    num1 INT;
+    num2 INT;
+    res int;
+BEGIN
+    
+      num1 := &NUmero_1;
+      num2 := &NUmero_2;
+    res:= num1+num2;
+    DBMS_OUTPUT.put_line ( num1 ||'+'||num2||'= '|| res);
+END;
+
+/*LERR DOS NUMERO E IMPRIMIREL MAYOR*/
+
+DECLARE
+    num1 INT;
+    num2 INT;
+    MAYOR int;
+BEGIN
+    
+    num1 := &NUmero_1;
+    num2 := &NUmero_2;
+    
+    IF num1>num2 THEN
+        MAYOR:= num1;
+        ELSE
+        MAYOR:= num2;
+    END IF;
+
+    DBMS_OUTPUT.put_line ( 'El mayo es: ' || mayor);
+END;
+
+
+/*MOSTRAR LOS PRIMERO 5 NUMERO NATURALES*/
+DECLARE -- Declaración y asignación de variables
+  counter NUMBER(6) := 0;
+BEGIN
+  LOOP
+    counter := counter + 1; -- Incrementamos la variable contador
+     DBMS_OUTPUT.PUT_LINE(counter || ' ');
+    -- Salimos del bucle cuando la condición de cumpla
+    EXIT WHEN counter>4;
+  END LOOP;
+
+END;
+
+/* Dos numero e imprimir si uno es multiplo del otro*/
+/* Tres numero y imprimir de mayor a menor*/
+/* Leer el radio de circuferencia e imprimir AREA y PERIMETRO*/
+/* Imprimier lo primero N numero de la serie de Fibonacci*/
+/* Factorial de un numero N*/
+/* Llenar un Vector  con N numeros ramdom( range 1 -  10 )  -> LA SUMA DEL VECTOR   */
+/*Llenar un Vector  con N numeros ramdom( range 1 -  100)  -> Imprimir vector de forma ordenada */
+
