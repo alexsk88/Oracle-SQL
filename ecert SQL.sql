1199dd03d9fafc4cf406331367a6a46db338680b
Alexander Nova <titoantifa69@gmail.com>
2020-10-27 13:10:45 -0500

    evalator_relations

--- ecert SQL.sql
+++ ecert SQL.sql
@@ -213,8 +213,8 @@ select
 	u.localization,
 	u.group,
 	te.nombre as 'nombre_evaluacion',
-    REPLACE(prta.name_question,',',';')'name_questions',
-	REPLACE(prta.name_anser,',',';' )'name_answer',
+    REPLACE(prta.name_question,',',';') 'name_questions',
+	REPLACE(prta.name_anser,',',';' ) 'name_answer',
     prta.status 'status',
     if(tha.user_answer = 0, '--','SELECCIONADA') 'rta_user'
 from thistoric_evaluations the
@@ -235,6 +235,63 @@ select  id, enunciado from questions;
 select  id,  REPLACE(REPLACE(enunciado, CHAR(13), ''), CHAR(10), '') from questions;
 select  id, respuesta from answers;
 
+## Mejorando comentarios evaluator relations
+
+create or replace view torails as (
+select 
+	er.assignment_id as 'assignment_id',
+	a.evaluator_id as 'evaluator_id',
+	u.id as 'user_id',
+	er.relation_id as 'relation_id',
+	s.project_id as 'project_id',
+	s.id as 'survey_id',
+	er.family_competence_id as 'family_competence_id',
+	er.competence_id as 'competence_id',
+	er.behavior_id as 'behavior_id',
+	er.score as 'score', 
+	er.ideal as 'ideal'
+from evaluator_relations er
+join assignments a ON er.assignment_id = a.id and a.status = 3
+join surveys s ON a.survey_id = s.id and s.project_id = 5
+inner join users u ON u.id = a.evaluated_id and u.status = 1 );
+
+        
+
+
+select 
+concat(u_ev.second_name,' ',u_ev.name) as 'nombre Evaluador',
+u_ev.identity,
+concat(u.second_name,' ',u.name) as 'nombre Evaluado',
+u.identity as 'identify eval',
+p.name as 'Cargo',
+l.name as 'Nivel',
+a.name as 'Area',
+g.name as 'Grupo',
+loc.name as 'Localizacion',
+com.name as 'Compañia',
+pro.name as 'Proyecto',
+s.name as 'Encuesta',
+re.name as 'Relacion',
+COALESCE(c.name, 'SIN COMPETENCIA' ) as 'Compencia',
+sq.statement as 'Pregunta',
+r1.score,
+r1.ideal,
+((r1.score*100)/r1.ideal) as '%'
+from report_1 r1
+left join users u ON u.id = r1.user_id
+left join users u_ev ON u_ev.id = r1.evaluator_id
+left join relations re ON re.id = r1.relation_id
+left join competences c ON c.id = r1.competence_id
+left join surveys s ON s.id = r1.survey_id
+left join positions p ON p.id = u.position_id
+left join survey_questions sq ON sq.id = r1.behavior_id
+left join levels l ON l.id = p.level_id
+left join areas a ON a.id = u.area_id
+left join groups g ON g.id = u.group_id
+left join localizations loc ON loc.id = u.localization_id
+left join companies com ON com.id = u.company_id
+left join projects pro ON pro.id = s.project_id
+order by r1.evaluator_id,r1.user_id, r1.family_competence_id;
 
 
 
